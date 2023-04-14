32618a082301400334ff22d6cc10a791cad282a4
yugomoro <130019797+yugomoro@users.noreply.github.com>
2023-04-14 16:38:27 +0900

    Update Nissan.php

--- Nissan.php
+++ Nissan.php
@@ -3,11 +3,16 @@
   require_once("Car.php");
   
   class Nissan extends Car {
+	private $poor_acceleration;
+	  
 	public function __construct() {
 		$this->company = "Nissan";
 		$this->price = mt_rand(100, 300);
 		$this->capacity = 5;
 		$this->acceleration = 8;
+		$this->passenger = mt_rand(1, $this->capacity);
+		$this->poor_acceleration = $this->acceleration * 0.6;
+		$this->actual_acceleration = $this->getActualAcceleration();
 	}
 }
 
