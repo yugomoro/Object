0ccede36e5cbc304f68338ac99ea63946023b01b
yugomoro <130019797+yugomoro@users.noreply.github.com>
2023-04-17 00:02:41 +0900

    Update Ferrari.php

--- Ferrari.php
+++ Ferrari.php
@@ -2,7 +2,7 @@
   require_once("Car.php");
 
 class Ferrari extends Car {
-	public function __construct() {
+	parent::public function __construct() {
 		$this->company = "Ferrari";
 		$this->price = mt_rand(2000, 5000);
 		$this->capacity = 2;
