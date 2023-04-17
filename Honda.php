0a3236a65f8c5fce8a5ac5ea6af690ffccad145c
yugomoro <130019797+yugomoro@users.noreply.github.com>
2023-04-18 06:35:04 +0900

    Update Honda.php

--- Honda.php
+++ Honda.php
@@ -1,15 +1,12 @@
 <?php
-  require_once("Car.php");
+require_once("Car.php");
 
-class Honda extends Car {
-	parent::public function __construct() {
-		$this->company = "Honda";
-		$this->price = mt_rand(400, 600);
-		$this->capacity = 8;
-		$this->acceleration = 5;
-		$this->passenger =mt_rand(1,$this->capacity);
-		$this->actual_acceleration = $this->getActualAcceleration();
-	}
+class Honda extends Car
+{
+  public function __construct()
+  {
+    parent::__construct("Honda", 400, 600, 8, 5, 120);
+  }
 }
 
 ?>
