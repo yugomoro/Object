6e188c26e425c126d2fb025d2343df095628afe2
yugomoro <130019797+yugomoro@users.noreply.github.com>
2023-04-18 06:41:05 +0900

    Create CarProduce.php

---
+++ CarProduce.php
@@ -0,0 +1,15 @@
+<?php
+
+class CarProduce
+{
+    private $cars = [Honda::class, Nissan::class, Ferrari::class];
+
+    public function generateRandomCar()
+    {
+        $rand = mt_rand(0, count($this->cars) - 1);
+        $class = $this->cars[$rand];
+        return new $class();
+    }
+}
+
+?>
