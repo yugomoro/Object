0b4c0b7f9f033dfb86071ada452e5fdd3bdfbd19
yugomoro <130019797+yugomoro@users.noreply.github.com>
2023-04-18 06:38:58 +0900

    Update Nissan.php

--- Nissan.php
+++ Nissan.php
@@ -1,19 +1,20 @@
 <?php
 
-  require_once("Car.php");
-  
-  class Nissan extends Car {
-	private $poor_acceleration;
-	  
-	parent::public function __construct() {
-		$this->company = "Nissan";
-		$this->price = mt_rand(100, 300);
-		$this->capacity = 5;
-		$this->acceleration = 8;
-		$this->passenger = mt_rand(1, $this->capacity);
-		$this->poor_acceleration = $this->acceleration * 0.6;
-		$this->actual_acceleration = $this->getActualAcceleration();
-	}
+require_once("Car.php");
+
+class Nissan extends Car
+{
+  public function __construct()
+  {
+    parent::__construct("Nissan", 100, 300, 5, 8, 120);
+  }
+
+  public function getActualAcceleration()
+  {
+    $actual_acceleration = parent::getActualAcceleration() * 0.6;
+    $text = "※Nissanは生産時の欠陥により、加速値の60％の性能しか出ません。";
+    return "Nissan乗車人数は" . $this->getPassenger() . "人のため、実際の加速性能は" . number_format($actual_acceleration, 2) . "m/s²です。" . $text;
+  }
 }
 
 ?>
