0ed2d8a0f990a6a01ce6d2f276dd5fb19bc35430
yugomoro <130019797+yugomoro@users.noreply.github.com>
2023-04-18 06:41:58 +0900

    Update Toyota.php

--- Toyota.php
+++ Toyota.php
@@ -1,14 +1,13 @@
 <?php
+
 require_once("Car.php");
 
-class Toyota extends Car {
-    parent::public function __construct() {
-        $this->company = "Toyota";
-        $this->price = mt_rand(500, 1000);
-        $this->capacity = 5;
-        $this->acceleration = $this->price / 100;
-        $this->passenger = mt_rand(1, $this->capacity);
-        $this->actual_acceleration = $this->getActualAcceleration();
-    }
+class Toyota extends Car
+{
+  public function __construct()
+  {
+    parent::__construct("Toyota", 500, 1000, 5, $this->price / 100, 150);
+  }
 }
+
 ?>
