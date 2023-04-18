6108bad344bfbd494d50b31d5897ba5902588c63
yugomoro <130019797+yugomoro@users.noreply.github.com>
2023-04-18 18:12:46 +0900

    Update Calculate.php

--- Calculate.php
+++ Calculate.php
@@ -1,71 +1,78 @@
+
 <?php
-  require_once("honda.php");
-  require_once("nissan.php");
-  require_once("ferrari.php");
 
-class Calculate {
-	private $honda_count = 0;
-	private $nissan_count = 0;
-	private $ferrari_count = 0;
-	private $total_count = 0;
-	private $total_price = 0;
-	private $average_price = 0;
+require_once("CarProduce.php");
 
-	public function __construct() {
-		 for ($i = 0; $i < mt_rand(10, 20); $i++) {
-            $car = $this->generateRandomCar();
-            switch ($car->getCompany()) {
-                case "Honda":
-                    $this->honda_count++;
-                    break;
-                case "Nissan":
-                    $this->nissan_count++;
-                    break;
-                case "Ferrari":
-                    $this->ferrari_count++;
-                    break;
+class Calculate
+{
+    private $honda_count = 0;
+    private $nissan_count = 0;
+    private $ferrari_count = 0;
+    private $total_count = 0;
+    private $total_price = 0;
+    private $average_price = 0;
+
+    public function __construct()
+    {
+        $carProduce = new CarProduce();
+        for ($i = 0; $i < mt_rand(10, 20); $i++) {
+            $car = $carProduce->generateRandomCar();
+            if ($car instanceof Honda) {
+                $this->honda_count++;
+            } elseif ($car instanceof Nissan) {
+                $this->nissan_count++;
+            } elseif ($car instanceof Ferrari) {
+                $this->ferrari_count++;
             }
             $this->total_count++;
             $this->total_price += $car->getPrice();
         }
         $this->average_price = $this->total_price / $this->total_count;
     }
-  
-  private function generateRandomCar() {
-	$rand = mt_rand(1,3);
-	switch ($rand) {
-		case 1:
-			return new Honda();
-		case 2:
-			return new Nissan();
-		case 3:
-			return new Ferrari();
-  }
-  }
-  
-  public function getHondaCount() {
-	return $this->honda_count;
-}
-  
-  public function getNissanCount() {
-	return $this->nissan_count;
-}
-  
-  public function getFerrariCount() {
-	return $this->ferrari_count;
-}
-  
-  public function getTotalCount() {
-	return $this->total_count;
-}
-  
-  public function getTotalPrice() {
-    return $this->total_price;
-}
-  
-  public function getAveragePrice() {
-	return $this->average_price;
-}
-}
 
-?>
+    public function getHondaCount()
+    {
+        return $this->honda_count;
+    }
+
+    public function getNissanCount()
+    {
+        return $this->nissan_count;
+    }
+
+    public function getFerrariCount()
+    {
+        return $this->ferrari_count;
+    }
+
+    public function getTotalCount()
+    {
+        return $this->total_count;
+    }
+
+    public function getTotalPrice()
+    {
+        return $this->total_price;
+    }
+
+    public function getAveragePrice()
+    {
+        return $this->average_price;
+    }
+
+    public function defineDistance()
+    {
+        $disstance = mt_rand(300000, 500000);
+        echo "レースの距離は" . number_format($disstance) . "mです。\n";
+        return $disstance;
+    }
+
+    public function convertTime($time)
+    {
+        $hours = floor($time / 3600);
+        $minutes = floor(($time / 60) % 60);
+        $seconds = $time % 60;
+
+        return $hours . "時間" . $minutes . "分" . $seconds . "秒";
+    }
+}
