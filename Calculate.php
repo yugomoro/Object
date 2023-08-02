4b0fe20ed31a0f1d1b81723e10dc7d1440040424
yugomoro <yugo.moro@tokyo.supersoftware.co.jp>
2023-08-03 00:59:14 +0900

    Calculateは計算のみ行うように修正

--- Calculate.php
+++ Calculate.php
@@ -1,61 +1,46 @@
 <?php
 
-require_once("CarProduce.php");
-
 class Calculate
 {
-    private $honda_count = 0;
-    private $nissan_count = 0;
-    private $ferrari_count = 0;
-    private $total_count = 0;
-    private $total_price = 0;
-    private $average_price = 0;
-
-    public function __construct()
+    public static function calculateTotalPrice(CarProduce $car_produce)
     {
-        $carProduce = new CarProduce();
-        for ($i = 0; $i < mt_rand(10, 20); $i++) {
-            $car = $carProduce->generateRandomCar();
-            if ($car instanceof Honda) {
-                $this->honda_count++;
-            } elseif ($car instanceof Nissan) {
-                $this->nissan_count++;
-            } elseif ($car instanceof Ferrari) {
-                $this->ferrari_count++;
-            }
-            $this->total_count++;
-            $this->total_price += $car->getPrice();
+        $total_price = 0;
+        $cars_generated = $car_produce->getCarsGenerated();
+        foreach ($cars_generated as $car) {
+            $total_price += $car->getPrice();
         }
-        $this->average_price = $this->total_price / $this->total_count;
+        return $total_price;
     }
 
-    public function getHondaCount()
+    public static function calculateAveragePrice(CarProduce $car_produce)
     {
-        return $this->honda_count;
+        $average_price = 0;
+        $total_price = self::calculateTotalPrice($car_produce);
+        $total_count = $car_produce->getTotalCount();
+        $average_price = $total_price / $total_count;
+        return round($average_price, 2);
     }
 
-    public function getNissanCount()
+    public static function formatJapaneseCurrency($amount)
     {
-        return $this->nissan_count;
+        if ($amount >= 10000) {
+            $man = floor($amount / 10000);
+            $yen = $amount % 10000;
+            $formatted = ($man > 0 ? "{$man}億" : '') . ($yen > 0 ? "{$yen}" : '') . '万円';
+        } else {
+            $formatted = "{$amount}万円";
+        }
+        return $formatted;
     }
 
-    public function getFerrariCount()
+    public static function outputQ4(CarProduce $car_produce)
     {
-        return $this->ferrari_count;
-    }
+        $total_price = self::calculateTotalPrice($car_produce);
+        $total_price_formatted = self::formatJapaneseCurrency($total_price);
+        $average_price = self::calculateAveragePrice($car_produce);
+        $average_price_formatted = self::formatJapaneseCurrency($average_price);
 
-    public function getTotalCount()
-    {
-        return $this->total_count;
-    }
-
-    public function getTotalPrice()
-    {
-        return $this->total_price;
-    }
-
-    public function getAveragePrice()
-    {
-        return $this->average_price;
+        echo "合計金額: {$total_price_formatted}\n";
+        echo "平均金額: {$average_price_formatted}\n";
     }
 }
